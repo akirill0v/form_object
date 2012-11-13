0ade8d68ae5bd789e41ce7b0ac2e1fa2d19f9fd8
Kirillov Alexander <saratovsource@gmail.com>
2012-11-14 00:27:59 +0400

    add some simple test for validate my filter form

--- test/lib/form_object_test.rb
+++ test/lib/form_object_test.rb
@@ -2,10 +2,24 @@ require 'test_helper'
 
 class FormObjectTest < TestCase
   def setup
-    @form = Filter.new
+    @form = Filter.new(query: "find me")
   end
 
   def test_should_form_not_persisted
     assert !@form.persisted?
   end
+
+  def test_form_should_have_query_attribute
+    assert "find me", @form.query
+  end
+
+  def test_should_be_valid_form
+    assert @form.valid?
+  end
+
+  def test_should_be_invalid_with_empty_query
+    @form.query = ""
+    assert !@form.valid?
+  end
+
 end
--- test/support/filter.rb
+++ test/support/filter.rb
@@ -1,3 +1,5 @@
 class Filter < FormObject::Base
-  
+  attribute :query, String
+
+  validates :query, presence: true
 end
