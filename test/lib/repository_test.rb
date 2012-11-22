0d1c5eabcb061f541a3aaf59cd6e4f1ef0b5e369
Kirillov Alexander <saratovsource@gmail.com>
2012-11-22 23:02:43 +0400

    add repository test to fetch form name for given form class

--- test/lib/repository_test.rb
+++ test/lib/repository_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RepositoryTest < TestCase
 
   def test_should_have_base_auth_in_collection
-    
+    assert_equal :base_auth, FormObject::Repository.instance[BaseAuthForm]
   end
 
 end
